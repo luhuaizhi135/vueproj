8d924cd1fa7d354cca48e202c6d0dd29cdec72ad
luhuaizhi <luhuaizhi@163.com>
2022-04-07 13:22:57 +0800

    add 013

--- app/src/pages/Home/index.vue
+++ app/src/pages/Home/index.vue
@@ -1,12 +1,34 @@
 <template>
   <div>
     <TypeNav />
+    <ListContainer/>
+    <Recommand/>
+    <Rank/>
+    <Like/>
+    <Floor/>
+    <Floor/>
+    <Brand/>
   </div>
 </template>
 
 <script>
+import ListContainer from './ListContainer'
+import Recommand from './Recommand'
+import Rank from './Rank'
+import Like from './Like'
+import Floor from './Floor'
+import Brand from './Brand'
+
 export default {
   name: "Home",
+  components:{
+    ListContainer,
+    Recommand,
+    Rank,
+    Like,
+    Floor,
+    Brand
+  }
 };
 </script>
 
